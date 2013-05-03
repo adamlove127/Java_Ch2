9c3ffce3716915cd89698acf87291fca7566f13b
adamlove127 <adam.love127@gmail.com>
2013-05-03 20:49:43 +1000

    Added Pythag application

--- Assignment2/PythagoreanTheorem.java
+++ Assignment2/PythagoreanTheorem.java
@@ -10,6 +10,21 @@ import acm.program.*;
 
 public class PythagoreanTheorem extends ConsoleProgram {
 	public void run() {
+		println("Enter values to compute the Phytagorean theorem");
+		double a = readDouble("a: ");
+		double b = readDouble("b: ");
+		double asqr = a*a;
+		double bsqr = b*b;
+		println();
+		println(asqr);
+		println(bsqr);
+		double addition = asqr + bsqr;
+		println(addition);
+		println();
+		double pythag = Math.sqrt(addition);
+		println("C = " + pythag);
+		
+		
 		/* You fill this in */
 	}
 }
